6134c791c77197fa783e79c8e097c11a6939e529
ChristianNomena <christiangrzk@gmail.com>
2020-09-12 12:10:28 +0300

    Object falling one instance

---
+++ Object/Object.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var speed = 200
+var velocity = Vector2.ZERO
+
+func _ready():
+	randomize()
+	self.create(Vector2(rand_range(32, 992), -128))
+
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	var collision = move_and_collide(self.velocity * delta)
+	if collision:
+		self.queue_free()
+
+func create(position):
+	self.position = position
+	self.velocity = Vector2(0, speed)
